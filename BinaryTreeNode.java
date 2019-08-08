1f1e8271a7bd2a73d8fa5c9b3fbf46295a881dbe
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-08 08:51:18 -0400

    made the binary tree node a subclass in the binary tree

--- BinaryTreeNode.java
+++ BinaryTreeNode.java
@@ -1,7 +1,9 @@
-public class BinaryTreeNode<T extends Comparable<T>> {
+public class BinaryTree<T extends Comparable<T>> {
 
-	public T data;
-	public BinaryTreeNode<T> left, right;
+	class TreeNode {
+		T data;
+		TreeNode<T> left, right;
+	}
 
 	public static void main(String[] args) {
 
