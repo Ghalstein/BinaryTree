0edd7ad246748014d570050475e401c7b39fd9ec
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-09 20:04:10 -0400

    created append for the BT

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,11 +1,13 @@
 public class BinaryTree<T extends Comparable<T>> {
 
+	// top node/ head of the tree
 	private TreeNode root = null;
 
 	public BinaryTree(T data) {
 		this.root = new TreeNode(data);
 	}
 
+	// inner class of BinaryTree that consists of the nodes of the tree
 	class TreeNode {
 		TreeNode(T data) {
 			this.data = data;
@@ -14,11 +16,41 @@ public class BinaryTree<T extends Comparable<T>> {
 		TreeNode left, right = null;
 	}
 
+	// appends the node to the endd of a leaf
 	private void append(T newData, TreeNode node) {
-		
+		// tersts whether the data gets passed to the right or left
+		if (newData.compareTo(node.data) > 0) {
+			if (node.right != null) {
+				append(newData, node.right);
+			}
+			else {
+				node.right = new TreeNode(newData);
+			}
+		}
+		else if ((newData.compareTo(node.data) < 0)) {
+			if (node.left != null) {
+				append(newData, node.left);
+			}
+			else {
+				node.left = new TreeNode(newData);
+			}
+		}
+		// incase the data is equal to the node then have to send it as close as possible
+		else {
+			if (node.left != null) {
+				TreeNode curr = node.left;
+				while (curr.right != null) {
+					curr = curr.right;
+				}
+				curr.right = new TreeNode(newData);
+			}
+			else {
+				node.left = new TreeNode(newData);
+			}
+		}
 	}
 
-
+	// encapsulates the append code
 	public void append(T newData) {
 		append(newData, root);
 	}
